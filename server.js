201226bf684a4fa792f1f8be383c90af8548930a
Smeet <smeetramteke21@gmail.com>
2025-03-01 20:28:17 +0530

    Updating gitignore

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
-
+connectDB();
 app.get('/', (req, res) => res.send('API Runnning'));
 
-
-const PORT = process.env.PORT || 5000
-
-app.listen(PORT, () => console.log(`server started on ${PORT}`));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Started server on port ${PORT}`));
